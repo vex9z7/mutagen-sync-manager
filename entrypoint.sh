99f9935818e10536256a840ce19fe5daaab0aad9
vex9z7 <face.tubas0p@icloud.com>
2026-04-05 23:22:17 +0900

    Add default SSH config generation

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ PUID="${PUID:-10001}"
 PGID="${PGID:-10001}"
 HOME_DIR="/home/mutagen"
 SSH_DIR="${HOME_DIR}/.ssh"
+SSH_CONFIG_PATH="${SSH_DIR}/config"
 SSH_KEY_PATH="${SSH_DIR}/id_ed25519"
 SSH_PUB_PATH="${SSH_KEY_PATH}.pub"
 MUTAGEN_DIR="${HOME_DIR}/.mutagen"
@@ -21,9 +22,16 @@ fi
 
 chown mutagen:mutagen "${HOME_DIR}" "${SSH_DIR}" "${MUTAGEN_DIR}"
 
-export SSH_DIR SSH_KEY_PATH SSH_PUB_PATH MUTAGEN_DIR
+export SSH_DIR SSH_CONFIG_PATH SSH_KEY_PATH SSH_PUB_PATH MUTAGEN_DIR
 
 exec gosu mutagen sh -c '
+if [ ! -f "${SSH_CONFIG_PATH}" ]; then
+  cat > "${SSH_CONFIG_PATH}" <<EOF
+Host *
+  IdentityFile /home/mutagen/.ssh/id_ed25519
+  IdentitiesOnly yes
+EOF
+fi
 if [ ! -f "${SSH_KEY_PATH}" ]; then
   ssh-keygen -t ed25519 -N "" -f "${SSH_KEY_PATH}" >/dev/null
 fi
